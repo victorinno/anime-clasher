f2e3bdc8495532819d3793e901c8020f772134c1
Floriano Peixoto <floriano.peixoto@services4you.com.br>
2017-03-30 11:03:07 -0300

    init

---
+++ ac/src/main/java/br/com/ac/AcApplication.java
@@ -0,0 +1,12 @@
+package br.com.ac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcApplication.class, args);
+	}
+}
